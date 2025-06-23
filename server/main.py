170873ba04a1191856dee21fc200cd3e6cffc921
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-06-23 14:35:56 -0400

    added `fastapi` dependency to `uv` project

--- server/main.py
+++ server/main.py
@@ -1,21 +1,21 @@
-# from fastapi import FastAPI
-# from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 from ai.agents import AgentManager, get_latest_agent_msg
 
-# app = FastAPI()
+app = FastAPI()
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
-# class UserRequest(BaseModel):
-#     user_message: str
+class UserRequest(BaseModel):
+    user_message: str
 
-# AGENT_MANAGER = AgentManager()
+AGENT_MANAGER = AgentManager()
 
-# @app.post("/")
-# async def recieve_user_input(user_req: UserRequest):
-#     resp = AGENT_MANAGER.invoke_main_with_text(user_req.user_message)
-#     return {"message": resp}
+@app.post("/")
+async def recieve_user_input(user_req: UserRequest):
+    resp = AGENT_MANAGER.invoke_main_with_text(user_req.user_message)
+    return {"message": resp}
