5d69490f9134253e35549759b207413316af0991
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-07-13 14:12:21 -0400

    added react-markdown and tailwind css dependencies

--- client/agent-orchestrator-client/src/pages/chat/components/Message.tsx
+++ client/agent-orchestrator-client/src/pages/chat/components/Message.tsx
@@ -1,4 +1,5 @@
 import type { Message } from "../message";
+import Markdown from "react-markdown";
 
 type MessageComponentProps = {
     message: Message
@@ -10,10 +11,17 @@ export default function MessageComponent({ message }: MessageComponentProps) {
     let msgContent;
 
     if (message.kind === "ai_message") {
-        msgContent = <p>{`${message.agent_name}: ${message.content}`}</p>
+        msgContent = (
+            <div className="message-ai">
+                <p>{message.agent_name}:</p>
+                <Markdown>{message.content}</Markdown>
+            </div>
+        );
     }
     else if (message.kind === "human_message") {
-        msgContent = <p>{`${message.username}: ${message.content}`}</p>
+        msgContent = (
+            <div className="message-human">{`${message.username}: ${message.content}`}</div>
+        );
     }
     else if (message.kind === "side_effect" && message.side_effect_kind === "image_generation") {
         msgContent = <p><img src={`data:image/jpeg;base64,${message.base64_encoded_image}`} width={"50%"} alt="AI generated image" /></p>
