f2e943cb65d044554a12d9e2e3719189c7aeaa60
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-09-27 15:01:07 -0400

    added documentation to the `auth` and `chat` service modules

--- server/auth/services.py
+++ server/auth/services.py
@@ -7,6 +7,12 @@ from auth.auth import authenticate_user, create_and_set_access_token, try_create
 
 
 def try_login_user(db: Session, response: Response, username: str, password: str) -> None:
+    """
+    Logs in the user using the provided credentials (:py:attr:`username` and :py:attr:`password`). 
+    If successful, the JWT token used for authentication is stored in an HTTP-only cookie. Raises a 
+    :py:class:`fastapi.HTTPException` exception if the provided credentials are invalid.
+    """
+
     user = authenticate_user(db, username, password)
     if not user:
         raise HTTPException(
@@ -20,6 +26,11 @@ def try_login_user(db: Session, response: Response, username: str, password: str
 
 
 def try_register_and_login_user(db: Session, response: Response, new_user_schema: CreateNewUser) -> None:
+    """
+    Registers a new user using the provided credentials in the :py:attr:`new_user_schema`. Logs in the user 
+    if registration was successful, raises the :py:class:`fastapi.HTTPException` exception otherwise.
+    """
+
     user = try_create_user_with_default_settings(db, new_user_schema)
 
     # Logs in the user.
@@ -27,4 +38,8 @@ def try_register_and_login_user(db: Session, response: Response, new_user_schema
 
 
 def logout_user(response: Response) -> None:
+    """
+    Logs out the user by deleting the JWT token from the user's HTTP-only cookie.
+    """
+
     response.delete_cookie("access_token")
--- server/chat/services.py
+++ server/chat/services.py
@@ -10,10 +10,16 @@ from chat.schemas import CreateNewChat, Chat, UserTextRequest
 from sqlalchemy.orm import Session
 
 def get_all_user_chat_schemas(user: UserTable) -> Sequence[Chat]:
+    """
+    Get all the chats belonging to the given user as a sequence of :py:class:`chat.schemas.Chat` chat schemas.
+    """
     return [chat_schema_from_db(chat) for chat in user.chats]
 
 
 def create_and_return_new_chat_for_user(db: Session, manager_store: AgentMangerInMemoryStore, new_chat_schema: CreateNewChat, user: UserTable) -> Chat:
+    """
+    Create a new chat using the provided chat creation data :py:attr:`new_chat_schema`. Returns the schema of the newly created chat.
+    """
     new_chat_from_db = initialize_new_chat_for_user(
         db, 
         manager_store, 
@@ -24,6 +30,10 @@ def create_and_return_new_chat_for_user(db: Session, manager_store: AgentMangerI
 
 
 def try_delete_chat_for_user(db: Session, manager_store: AgentMangerInMemoryStore, chat_id: uuid.UUID, user: UserTable) -> bool:
+    """
+    Tries deleting the chat with the given ID for the given user. Raises the :py:class:`fastapi.HTTPException` exception if 
+    an issue occurs, such as the given ID not existing in the database or the chat not belonging to the given user.
+    """
     chat = get_chat_by_id_from_user_throwing(db, user, chat_id)
     could_delete = delete_chat(db, manager_store, user, chat)
     return could_delete
@@ -35,6 +45,10 @@ def get_full_trace_schema_history_for_user_chat(
     chat_id: uuid.UUID, 
     user: UserTable
 ) -> Sequence[Trace]:
+    """
+    Returns the entire chat history for the given chat for the given user as a sequence of trace schemas. 
+    Raises the :py:class:`fastapi.HTTPException` exception if an issue occurs.
+    """
     chat = get_chat_by_id_from_user_throwing(db, user, chat_id)
 
     agent_manager = get_or_init_agent_manager_for_chat(db, manager_store, user, chat)
@@ -48,6 +62,10 @@ def get_trace_schemas_after_timestamp_for_user_chat(
     user: UserTable, 
     timestamp: float,
 ) -> Sequence[Trace]:
+    """
+    Similar to the :py:func:`chat.services.get_full_trace_schema_history_for_user_chat` service, except that 
+    it only returns the traces created after the provided timestamp.
+    """
     chat = get_chat_by_id_from_user_throwing(db, user, chat_id)
     
     agent_manager = get_or_init_agent_manager_for_chat(db, manager_store, user, chat)
@@ -61,6 +79,10 @@ def invoke_agent_manager_for_chat_with_text(
     current_user: UserTable, 
     user_request: UserTextRequest,
 ) -> None:
+    """
+    Invokes the agent manager associated with the chat with the given ID for the given user. Uses the 
+    contents of the provided :py:attr:`user_request` to generate traces from the agent manager.
+    """
     chat = get_chat_by_id_from_user_throwing(db, current_user, chat_id)
     agent_manager = get_or_init_agent_manager_for_chat(db, manager_store, current_user, chat)
     _ = agent_manager.invoke_main_agent_with_text(current_user.username, user_request.user_message, db)
