64e15eb98eea2f12cbf235542a879c6ea83ac2e1
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-11-10 20:59:37 -0400

    feat: theme can now be changed

--- client/agent-orchestrator-client/src/pages/themes/Themes.tsx
+++ client/agent-orchestrator-client/src/pages/themes/Themes.tsx
@@ -1,7 +1,73 @@
-import { useTheme } from "@emotion/react";
+import { CUSTOM_THEMES, useCustomTheme, type CustomThemeKind } from "./useCustomTheme";
+import { Box, Button, Card, CardContent, Container, Typography } from "@mui/material";
+
+type SquareProps = {
+    color: string,
+};
+
+function Square({ color }: SquareProps) {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box
+        sx={{
+          width: 40,
+          height: 40,
+          bgcolor: color, 
+          borderRadius: 1,
+        }}
+      />
+    </Box>
+  );
+}
 
 export default function Themes() {
-    const foo = useTheme();
+    const { customThemeKind, setCustomThemeKind } = useCustomTheme()!;
 
-    return <p>Hello</p>;
+    console.log(customThemeKind);
+
+    return (
+        <Container maxWidth="sm">
+            <Card>
+                <CardContent sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center"
+                }}>
+                    <Typography 
+                        variant="h3"
+                        align="center"
+                        sx={{ mb: 5 }}
+                    >
+                        Set Theme
+                    </Typography>
+
+                    {Object.keys(CUSTOM_THEMES).map(kind => (
+                        <Box 
+                            key={kind}
+                            sx={{
+                                display: "flex",
+                                width: "100%",
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                }}
+                            >
+                                <Square color={CUSTOM_THEMES[kind as CustomThemeKind].mainColor} />
+                                <Square color={CUSTOM_THEMES[kind as CustomThemeKind].backgroundColor} />
+                            </Box>
+                            <Button
+                                variant="outlined"
+                                onClick={() => setCustomThemeKind(kind as CustomThemeKind)}
+                            >
+                                {kind}
+                            </Button>
+                        </Box>
+                    ))}
+                </CardContent>
+
+            </Card>
+        </Container>
+    );
 }
--- client/agent-orchestrator-client/src/pages/themes/useCustomTheme.tsx
+++ client/agent-orchestrator-client/src/pages/themes/useCustomTheme.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "@emotion/react";
 import { createTheme, responsiveFontSizes } from "@mui/material";
 import { blue, purple, red, yellow } from "@mui/material/colors";
 import type { Theme } from "@mui/material/styles";
-import { createContext, useContext, useState, type ReactNode } from "react";
+import { createContext, useContext, useEffect, useState, type ReactNode } from "react";
 
 type CustomTheme = {
     mainColor: string,
@@ -11,20 +11,20 @@ type CustomTheme = {
     mode?: 'light' | 'dark',
 };
 
-type CustomThemeKind = "crimson-gold" | "light" | "dark";
+export type CustomThemeKind = "gold-crimson" | "light-blue" | "dark-purple";
 
 export const CUSTOM_THEMES: Record<CustomThemeKind, CustomTheme> = {
-    "crimson-gold": {
+    "gold-crimson": {
         mainColor: red[500],
         paperColor: yellow[100],
         backgroundColor: yellow[50],
     },
-    "light": {
+    "light-blue": {
         mainColor: blue[900],
         paperColor: "#e0e0e0ff",
         backgroundColor: "#ffffff",
     },
-    "dark": {
+    "dark-purple": {
         mainColor: purple[300],
         paperColor: "#161616ff",
         backgroundColor: "#000000",
@@ -55,8 +55,34 @@ function createMUIThemeFromCustomTheme(customTheme: CustomTheme): Theme {
     }));
 }
 
+const CUSTOM_THEME_STORAGE_KEY = "agent-orchestrator-custom-theme-kind";
+
+const DEFAULT_CUSTOM_THEME_KIND: CustomThemeKind = "gold-crimson";
+
 export function CustomThemeProvider({ children }: { children: ReactNode }) {
-    const [customThemeKind, setCustomThemeKind] = useState<CustomThemeKind>("crimson-gold");
+    const [customThemeKind, setCustomThemeKind] = useState<CustomThemeKind>(() => {
+        try {
+            const storedThemeKind = localStorage.getItem(CUSTOM_THEME_STORAGE_KEY);
+            
+            if (! Object.hasOwn(CUSTOM_THEMES, storedThemeKind as CustomThemeKind)) {
+                return DEFAULT_CUSTOM_THEME_KIND;
+            }
+            return storedThemeKind as CustomThemeKind;
+        }
+        catch (err) {
+            console.error("Error while saving app theme: ", err);
+            return DEFAULT_CUSTOM_THEME_KIND;
+        }
+    });
+
+    useEffect(() => {
+        try {
+            localStorage.setItem(CUSTOM_THEME_STORAGE_KEY, customThemeKind);
+        }
+        catch (err) {
+            console.error("Error while saving app theme: ", err);
+        }
+    }, [customThemeKind]);
 
     const value = {
         customThemeKind,
